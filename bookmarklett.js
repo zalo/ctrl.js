1624af50c3d4ee6f84ae0ba4b900dff6ff02a71e
Johnathon Selstad <makeshifted@gmail.com>
2019-07-03 21:36:42 -0700

    To Undo

--- bookmarklett.js
+++ bookmarklett.js
@@ -2,7 +2,7 @@
 // javascript:var s=document.createElement("script");s.src="https://zalo.github.io/ctrl.js/bookmarklet.js",document.body.appendChild(s);
 
 setTimeout(()=>{
-  var i, s, ss = ['https://zalo.github.io/ctrl.js/peerjs.min.js', 'https://zalo.github.io/ctrl.js/qrcode.min.js', 'https://zalo.github.io/ctrl.js/Injector.js'];
+  var i, s, ss = ['https://zalo.github.io/ctrl.js/peerjs.min.js', 'https://zalo.github.io/ctrl.js/qrcode.min.js', 'https://zalo.github.io/ctrl.js/injector.js'];
   //var i, s, ss = ['peerjs.min.js', 'qrcode.min.js', 'Injector.js']; // The local testing version...
   for (i = 0; i != ss.length; i++) {
     s = document.createElement('script');

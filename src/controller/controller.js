24efa9412d83feb0733997f3e1198bccd8a66625
Johnathon Selstad <makeshifted@gmail.com>
2019-07-06 16:43:19 -0700

    Add a joystick toggling button... best of both worlds!

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,7 +3,7 @@ var CreateCtrlJsController = function () {
   this.lastTimeRendered = 0.0;
   this.viewDirty = true;
   this.moveArrowButtons = false;
-  this.useJoystick = false;
+  this.useJoystick = null;
 
   this.currentTouches = {};
 
@@ -49,22 +49,7 @@ var CreateCtrlJsController = function () {
       // Name the buttons so they can properly give off events later
       this.A.btnName = "A"; this.B.btnName = "B"; this.Start.btnName = "Start"; 
 
-      if(this.useJoystick){
-        this.joystick = new THREE.Group();
-        this.joystick.position.set( -11, -4, 2.5 );
-        this.joystick.add(this.createWhiteSphere(null,  0, 0, 5.0,  0.125, 0.125, 0.05,  8, 0x777777));
-        this.joystick.add(this.createWhiteSphere(null,  0, 0, 2.5,  0.02, 0.02, 0.14,  8, 0x777777));
-        this.buttons.push(this.joystick);
-        this.scene.add(this.joystick);
-        this.Up = {btnName: "Up"}; this.Down = {btnName: "Down"}; this.Left = {btnName: "Left"}; this.Right = {btnName: "Right"};
-      } else {
-        this.joystick = null;
-        this.Up     = this.createWhiteSphere(this.buttons, -12 ,  0, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, "/\\");
-        this.Down   = this.createWhiteSphere(this.buttons, -12 , -8, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, "v");
-        this.Left   = this.createWhiteSphere(this.buttons, -16 , -4, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, "<");
-        this.Right  = this.createWhiteSphere(this.buttons, -8  , -4, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, ">");
-        this.Up.btnName = "Up"; this.Down.btnName = "Down"; this.Left.btnName = "Left"; this.Right.btnName = "Right";
-      }
+      this.toggleDPad();
     } );
 
     // Handle Touch Events
@@ -78,6 +63,43 @@ var CreateCtrlJsController = function () {
     this.curCanvas.addEventListener( 'touchcancel',      this.onTouchEnd,    false );
     this.curCanvas.addEventListener( 'touchend',         this.onTouchEnd,    false );
     this.curCanvas.addEventListener( 'gesturechange',    this.onTouchEnd,    false );
+  }
+
+  this.toggleDPad = function(){
+    // Clean-up old directional input if applicable
+    if(this.useJoystick !== null){
+      if (this.useJoystick) {
+        this.scene.remove(this.joystick);
+        //this.buttons.remove(this.joystick);
+        this.buttons.splice(this.buttons.indexOf(this.joystick), 1);
+        this.joystick = null;
+        this.useJoystick = false;
+      } else {
+        this.scene.remove(this.Up);    this.buttons.splice(this.buttons.indexOf(this.Up), 1);//this.buttons.remove(this.Up);
+        this.scene.remove(this.Down);  this.buttons.splice(this.buttons.indexOf(this.Down), 1);//this.buttons.remove(this.Down);
+        this.scene.remove(this.Left);  this.buttons.splice(this.buttons.indexOf(this.Left), 1);//this.buttons.remove(this.Left);
+        this.scene.remove(this.Right); this.buttons.splice(this.buttons.indexOf(this.Right), 1);//this.buttons.remove(this.Right);
+        this.useJoystick = true;
+      }
+    } else { this.useJoystick = true; }
+    if(this.useJoystick){
+      this.joystick = new THREE.Group();
+      this.joystick.position.set( -11, -4, 2.5 );
+      this.joystick.add(this.createWhiteSphere(null,  0, 0, 5.0,  0.125, 0.125, 0.05,  8, 0x777777));
+      this.joystick.add(this.createWhiteSphere(null,  0, 0, 2.5,  0.02, 0.02, 0.14,  8, 0x777777));
+      this.buttons.push(this.joystick);
+      this.scene.add(this.joystick);
+      this.Up = {btnName: "Up"}; this.Down = {btnName: "Down"}; this.Left = {btnName: "Left"}; this.Right = {btnName: "Right"};
+    } else {
+      this.joystick = null;
+      this.Up     = this.createWhiteSphere(this.buttons, -12 ,  2, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, "/\\");
+      this.Down   = this.createWhiteSphere(this.buttons, -12 , -8, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, "v");
+      this.Left   = this.createWhiteSphere(this.buttons, -17 , -3, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, "<");
+      this.Right  = this.createWhiteSphere(this.buttons, -7  , -3, 3.0, 0.1, 0.1, 0.05, 8, 0x888888, ">");
+      this.Up.btnName = "Up"; this.Down.btnName = "Down"; this.Left.btnName = "Left"; this.Right.btnName = "Right";
+    }
+    this.viewDirty = true;
+    this.frameNumber = 0;
   }
 
   // Handle resizing
@@ -157,7 +179,7 @@ var CreateCtrlJsController = function () {
     event.preventDefault();
     event.stopPropagation();
     this.viewDirty = true;
-    //this.frameNumber = 0; // Forces an instant refresh
+    if(this.useJoystick) { this.frameNumber = 0; } // Forces an instant refresh
     this.handleTouches();
   }.bind(this);
   this.onTouchEnd = function(event){
@@ -219,7 +241,7 @@ var CreateCtrlJsController = function () {
           if(typeof this.buttons[closestButton].material !== 'undefined') {
             // Handle the button pressing logic
             this.buttons[closestButton].material.emissive.setHex( 0xffffff );
-            this.buttons[closestButton].material.emissiveIntensity = touch.force >= 1.0 ? -0.2 : -touch.force;
+            this.buttons[closestButton].material.emissiveIntensity = touch.force >= 1.0 ? -0.25 : touch.force * -0.25;
             this.handlePress(this.buttons[closestButton]);
           } else {
             // Handle the joystick logic
@@ -264,7 +286,7 @@ var CreateCtrlJsController = function () {
     // Set up a lazy render loop where it only renders if it's been interacted with in the last second
     // And even then, only every third frame to preserve the battery-life of the phone
     if (this.viewDirty) { this.lastTimeRendered = this.time.getElapsedTime(); this.viewDirty = false; }
-    if (this.time.getElapsedTime() - this.lastTimeRendered < 0.2 && this.frameNumber % 4 === 0) {
+    if (this.time.getElapsedTime() - this.lastTimeRendered < 0.2 && this.frameNumber % 2 === 0) {
       this.scene.background = this.ctrljs.disconnected ? new THREE.Color(0xff0000) : new THREE.Color(0x000000);
       this.renderer.render(this.scene, this.camera); 
     }
